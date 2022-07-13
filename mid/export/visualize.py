242effeb189658aba5be4d7cad5a494e7344630d
shilem2 <moshe.shilemay@medtronic.com>
2022-07-13 18:35:05 +0300

    wip export MisRefresh to coco keypoints format first visualization of coco like dataset using fiftyone

---
+++ mid/export/visualize.py
@@ -0,0 +1,31 @@
+import fiftyone as fo
+import fiftyone.zoo as foz
+
+def fo_quickstart():
+
+	dataset = foz.load_zoo_dataset("quickstart")
+	session = fo.launch_app(dataset)
+
+	pass
+
+def mmpose_coco_test():
+
+	labels_file = r"/Users/shilem2/data/human_pose_estimation/coco_overfit/all/test_coco.json"
+	dataset_file = r"/Users/shilem2/data/human_pose_estimation/coco_overfit/all_fiftyone"  # folder
+
+	dataset = fo.Dataset.from_dir(
+		dataset_type=fo.types.COCODetectionDataset,
+		label_types=["detections", "segmentations", "keypoints"],
+		dataset_dir=dataset_file,
+		labels_path=labels_file)
+
+	session = fo.launch_app(dataset)
+
+	pass
+
+if __name__ == '__main__':
+
+	# fo_quickstart()
+	mmpose_coco_test()
+
+	pass
