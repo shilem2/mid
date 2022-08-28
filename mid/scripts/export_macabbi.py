bb293f34d9a67b5f8d608236bed47c7bed33ebc3
shilem2 <a@b.c>
2022-08-28 10:29:03 +0000

    export maccabi 002: similar to 000, with clahe1 preprocessing

--- mid/scripts/export_macabbi.py
+++ mid/scripts/export_macabbi.py
@@ -20,18 +20,22 @@ sitk.ProcessObject_SetGlobalWarningDisplay(False)
 
 def export_maccabi_to_coco():
 
-    anns_type = 'vert_implant'  #'implant'  # one of {'implant', 'vert_implant'}
+    anns_type = 'implant'  #'implant'  # one of {'implant', 'vert_implant'}
     vert_visibility_flag = 0 if (anns_type == 'implant') else 2
     n_max_study_id = -1
-    img_processing_type = 'adjust_dynamic_range'
-    # img_processing_type = 'clahe1'
+    # img_processing_type = 'adjust_dynamic_range'
+    img_processing_type = 'clahe1'
     cfg_update = {'pixel_spacing_override': (1., 1.)}
     skip_flipped_anns = True  # some of the annotations are horizontally flipped
     projection_list = ['LT', 'AP']
     n_split_list = 9
 
-    output_dir_prefix = '001_'  # ''
-    output_dir_suffix = '_with_verts'  # ''
+    # 001
+    # output_dir_prefix = '001_'  # ''
+    # output_dir_suffix = '_with_verts'  # ''
+    # 002
+    output_dir_prefix = '002_'  # ''
+    output_dir_suffix = '_clahe1'  # ''
 
     # load dataset
     data_path = Path('/mnt/magic_efs/moshe/implant_detection/data/2022-08-10_merged_data_v2/')
