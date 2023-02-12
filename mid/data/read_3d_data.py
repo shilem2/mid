ca08030954ba91e750d937db4e6b87cadd09435b
moshe.shilemay <a@b.c>
2023-02-12 19:02:11 +0000

    wip ct db - generate ct metadata df - process it - add option to filter it by its columns - simple assigment of pre/post op boolean value by using xr 'first_procedure_date' - should improve

--- mid/data/read_3d_data.py
+++ mid/data/read_3d_data.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 from tqdm import tqdm
+import pandas as pd
 
 from mid.data import utils
 
@@ -42,4 +43,60 @@ def read_metadata_root_dir(root_dir, pattern='**/Patient.json'):
         metadata_dict[study_id].append(metadata)
 
     return metadata_dict
+
+
+def generate_metadata_df(root_dir, pattern='**/Patient.json', process_df_flag=True, num_max=-1, output_df_file=None):
+
+    paths = Path(root_dir).rglob(pattern)
+    # path_list = list(paths)
+
+    srs = []
+    for n, path in tqdm(enumerate(paths)):
+
+        metadata, study_id = read_metadata_single_dir(path.parent)
+        sr = pd.Series(metadata)
+        srs.append(sr)
+
+        if (num_max > 0) and (n >= (num_max - 1)):
+            break
+
+    df = pd.concat(srs, axis=1).transpose()
+
+    if process_df_flag:
+        df = process_df(df)
+
+    if output_df_file is not None:
+        df.to_parquet(output_df_file)
+
+    return df
+
+
+def process_df(df, relative_path_start=4, out_cols=['study_id', 'mongo_id', 'full_dir_path', 'relative_dir_path', 'dcm_date']):
+
+    df['study_id'] = df.study_id.astype(int)
+    df['relative_dir_path'] = df['dir_path'].apply(lambda x: '/'.join(x.split('/')[relative_path_start:]))
+    df['dcm_date'] = pd.to_datetime(df['series_date'], format='%Y-%m-%dT%H:%M:%S')
+    df['dcm_date'] = df['dcm_date'].apply(lambda x: x.date())
+    df.rename(columns={'dir_path': 'full_dir_path'}, inplace=True)
+
+    df = df[out_cols]
+
+    return df
+
+
+def filter_anns_df(df, study_id=None, mongo_id=None, relative_file_path=None, dcm_date=None):
+    """
+    Filter annotations data frame.
+    At least on of the arguments other than vert_df should be given.
+    None value means that filtering does not consider that argument.
+    """
+
+    inds = ((study_id is None) or (df.study_id == study_id)) & \
+           ((mongo_id is None) or (df.mongo_id == mongo_id)) & \
+           ((relative_file_path is None) or (df.relative_file_path == relative_file_path)) & \
+           ((dcm_date is None) or (df.dcm_date == dcm_date))
+
+    df_out = df[inds]
+
+    return df_out
 
--- mid/scripts/read_ct_metadata.py
+++ mid/scripts/read_ct_metadata.py
@@ -1,9 +1,11 @@
-import json
 from pathlib import Path
-from tqdm import tqdm
-
+import pandas as pd
 from mid.data import utils
-from mid.data.read_3d_data import read_metadata_single_dir, read_metadata_root_dir
+from mid.data.read_3d_data import read_metadata_single_dir, read_metadata_root_dir, generate_metadata_df, filter_anns_df
+from mid.data import MaccbiDataset
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
 
 
 def read_metadata_single_dir_example():
@@ -23,16 +25,76 @@ def read_metadata_root_dir_example():
     utils.save_compressed_pickle(metadata, metadata_file)
 
     metadata_file = 'm:/moshe/3d_prediction/results/3d_db/metadata.dat'
-    metadata2 = utils.load_compressed_pickle(metadata_file)
+    metadata = utils.load_compressed_pickle(metadata_file)
 
     pass
 
+
+def generate_metadata_df_example():
+
+    root_dir = 'm:/magic/output/Pre_post_CT_XR_cohort/'
+    num_max = 30
+    output_df_file = 'm:/moshe/3d_prediction/results/3d_db/metadata_df_{}.parquet'.format(num_max)
+
+    generate_metadata_df(root_dir, pattern='**/Patient.json', num_max=num_max, output_df_file=output_df_file)
+
+    pass
+
+def read_xr_data():
+
+    # data_path = Path('/mnt/magic_efs/moshe/implant_detection/data/2023-01-17_merged_data_v2/')
+    data_path = Path('M:/moshe/data/2023-01-17_merged_data_v2/')
+    vert_file = (data_path / 'vert' / 'vert.parquet').resolve().as_posix()
+    rod_file = (data_path / 'rod' / 'rod.parquet').resolve().as_posix()
+    screw_file = (data_path / 'screw' / 'screw.parquet').resolve().as_posix()
+    dicom_file = (data_path / 'dicom' / 'dicom.parquet').resolve().as_posix()
+
+    cfg_update_data = {'pixel_spacing_override': (1., 1.),  # None
+                       'pairs_for_registration': {'acquired_date': 'different',
+                                                  'skip_flipped_anns': False,
+                                                  'latest_preop': True,
+                                                  'latest_postop': False,
+                                                  'projection': 'LT',
+                                                  'body_pose': 'Neutral',
+                                                  },
+                       }
+
+    dataset = MaccbiDataset(vert_file=vert_file, rod_file=rod_file, screw_file=screw_file, dicom_file=dicom_file, cfg_update=cfg_update_data)
+
+    study_ids = dataset.get_study_id_list(key='vert_df')
+
+    study_id = 1003813  # 1 ct
+    study_id = 1023714  # 22 cts
+
+    study_df = dataset.filter_study_id(study_id)
+    df_xr = study_df.drop_duplicates('relative_file_path')
+
+    metadata_df_file = 'm:/moshe/3d_prediction/results/3d_db/metadata_df_30.parquet'
+    df_3d_full = pd.read_parquet(metadata_df_file)
+
+    df_3d = filter_anns_df(df_3d_full, study_id=study_id)
+
+    first_procedure_date_list = df_xr['first_procedure_date'].unique()
+    if len(first_procedure_date_list):
+        # FIXME: sort by date and take earliest
+        pass
+    else:
+        first_procedure_date = first_procedure_date_list[0]
+
+    first_procedure_date_str = first_procedure_date.strftime('%Y-%m-%d')
+
+    df_3d['is_preop'] = df_3d.dcm_date.apply(lambda x: x.strftime('%Y-%m-%d') < first_procedure_date_str)
+    df_3d['is_postop'] = df_3d.dcm_date.apply(lambda x: x.strftime('%Y-%m-%d') >= first_procedure_date_str)
+
+    pass
 
 
 
 if __name__ == '__main__':
 
     # read_metadata_single_dir_example()
-    read_metadata_root_dir_example()
+    # read_metadata_root_dir_example()
+    generate_metadata_df_example()
+    read_xr_data()
 
     pass
