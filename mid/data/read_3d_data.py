78875411b4ad3c26cb64c9abaf5c3f2a9cb776c9
moshe.shilemay <a@b.c>
2023-02-12 13:36:29 +0000

    wip ct db - refactor code

---
+++ mid/data/read_3d_data.py
@@ -0,0 +1,45 @@
+import json
+from pathlib import Path
+from tqdm import tqdm
+
+from mid.data import utils
+
+
+def read_metadata_single_dir(dir_path, patient_file='Patient.json', study_file='Study.json',
+                             study_analysis_file='StudyAnalysis.json'):
+    dir_path = Path(dir_path)
+
+    patient = json.load(open(dir_path / patient_file))
+    study = json.load(open(dir_path / study_file))
+    study_analysis = json.load(open(dir_path / study_analysis_file))
+
+    study_id = patient['patientId']  # Maccabi study id
+
+    metadata = {
+        'study_id': study_id,
+        'mongo_id': study['patientId'],  # Mongo DB id
+        'series_date': study['seriesDate'],
+        'dir_path': dir_path.absolute().as_posix(),
+        # 'study_analysis': study_analysis,
+    }
+
+    return metadata, study_id
+
+
+def read_metadata_root_dir(root_dir, pattern='**/Patient.json'):
+
+    paths = Path(root_dir).rglob(pattern)
+    # path_list = list(paths)
+
+    metadata_dict = {}
+    for path in tqdm(paths):
+
+        metadata, study_id = read_metadata_single_dir(path.parent)
+
+        if study_id not in metadata_dict:
+            metadata_dict[study_id] = []
+
+        metadata_dict[study_id].append(metadata)
+
+    return metadata_dict
+
