f861fdee702ef677edae6ce13aff2b976b0ac7f5
moshe.shilemay <a@b.c>
2023-02-21 17:51:39 +0000

    wip Maccabi3dDataset - fix bug in date string to datetime conversion - add support in lists of root dirs

--- mid/scripts/read_ct_metadata.py
+++ mid/scripts/read_ct_metadata.py
@@ -94,14 +94,18 @@ def generate_3d_meta_df_example():
     procedure_meta_file = 'm:/moshe/Maccabi_DB/postop_CT_WO_immediate.csv'
     # procedure_meta_file = 'm:/moshe/Maccabi_DB/postop_CT_all.csv'
 
-    meta_root_dir = 'm:/magic/output/Pre_post_CT_XR_cohort/'
+    # meta_root_dir = 'm:/magic/output/Pre_post_CT_XR_cohort/'
+    meta_root_dir = ['m:/magic/output/Pre_post_CT_XR_cohort/20220328072248/',
+                     'm:/magic/output/Pre_post_CT_XR_cohort/20220329090743/',
+                     'm:/magic/output/Pre_post_CT_XR_cohort/20220330060340/',
+                     ]
     output_dir_sfx = '_postop_ct_wo_immediate'
 
     # meta_root_dir = 'm:/magic/output/pre_post_ct_not_in_XR_cohort/'
     # output_dir_sfx = '_pre_post_ct_not_in_XR_cohort'
 
-    # num_max = -1
-    num_max = 50
+    num_max = -1
+    # num_max = 100
     output_df_file = 'm:/moshe/3d_prediction/results/3d_db/metadata_df_{}{}.parquet'.format(num_max if num_max > 0 else '', output_dir_sfx)
 
     df = generate_3d_meta_df(meta_root_dir, procedure_meta_file, output_df_file=output_df_file, pattern='**/Patient.json', num_max=num_max)
